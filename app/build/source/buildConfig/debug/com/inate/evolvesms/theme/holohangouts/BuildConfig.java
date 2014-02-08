0564ca8f4387b384e2c1bf69f908ebec58fc175b
Nate Gantt <nategantt@gmail.com>
2014-02-07 21:06:32 -0500

    Bunches of stuff
    Switched IDE to Android Studio
    Switched bubble colors
    Better font choice for date
    Optimized pngs

---
+++ app/build/source/buildConfig/debug/com/inate/evolvesms/theme/holohangouts/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.inate.evolvesms.theme.holohangouts;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "com.inate.evolvesms.theme.holohangouts";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 130;
+}
